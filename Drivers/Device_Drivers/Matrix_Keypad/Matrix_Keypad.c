a53e52d7c5b93f6a4106929ba1f060d3a87b69ea
Ramy Sorial <ramysorial92@gmail.com>
2024-02-16 11:10:43 +0200

    initial commit

---
+++ Drivers/Device_Drivers/Matrix_Keypad/Matrix_Keypad.c
@@ -0,0 +1,91 @@
+/*
+ * Matrix_Keypad.c
+ *
+ *  Created on: Jan 31, 2024
+ *      Author: ramys
+ */
+
+#include "Matrix_Keypad.h"
+
+
+/// @brief Reads the input status from the input port
+static uint16_t Keypad_GetInputs(Matrix_Keypad_t *keypad) {
+	uint16_t Input = keypad->IntputPort->IDR & keypad->IP_Mask;
+	return (Input >> keypad->InputStartingPin);
+}
+
+///@brief Write the status of output ports
+static void Keypad_SetOutputs(Matrix_Keypad_t *keypad,
+								uint16_t OutputStat) {
+	//turn off the pins
+	keypad->OutputPort->BRR |= keypad->OP_Mask;
+	//turn on only the needed ones according to the required output
+	keypad->OutputPort->BSRR |= (OutputStat << keypad->OutputStartingPin);
+}
+
+/// @fn void Keypad_Delay(uint32_t)
+/// @brief
+/// Halts the program execution for a certain time
+/// @param delay_ms delay value in milliseconds
+static void Keypad_Delay(uint32_t delay_ms) {
+	HAL_Delay(delay_ms);
+}
+
+void Keypad_Refresh(Matrix_Keypad_t *keypad) {
+// temporary variable to store the new status of keys
+	uint32_t TempKeys = 0;
+	uint16_t OutputStat = 1 << (keypad->Rows - 1);
+
+	uint16_t x = 0;
+	do {
+
+		TempKeys <<= keypad->Columns;
+		// send the output status to the hardware
+		Keypad_SetOutputs(keypad, OutputStat);
+		Keypad_Delay(1);
+		// read the input status from the hardware
+
+		TempKeys |= Keypad_GetInputs(keypad);
+		OutputStat >>= 1;
+		x++;
+	} while (x < keypad->Rows);
+
+	keypad->KeysStatus = TempKeys;
+}
+
+void Keypad_Init(Matrix_Keypad_t *keypad) {
+	GPIO_InitTypeDef config;
+
+//put a logic 1 in every position where an input pin is selected
+	uint16_t Pins = 0;
+
+//configure the columns gpio pins as inputs
+//enable pull down resistors to eliminate the use of external ones
+	for (int x = 0; x < keypad->Columns; x++) {
+		Pins |= (1 << (x + keypad->InputStartingPin));
+	}
+//Load the input pin Mask
+	keypad->IP_Mask = Pins;
+
+	config.Mode = GPIO_MODE_INPUT;
+	config.Pull = GPIO_PULLDOWN;
+	config.Pin = Pins;
+	HAL_GPIO_Init(keypad->IntputPort, &config);
+
+//configure the rows gpio pins as outputs
+	Pins = 0;
+	for (int x = 0; x < keypad->Rows; x++) {
+		Pins |= (1 << (x + keypad->OutputStartingPin));
+	}
+
+//Load the output pin Mask
+	keypad->OP_Mask = Pins;
+
+	config.Mode = GPIO_MODE_OUTPUT_PP;
+	config.Speed = GPIO_SPEED_FREQ_LOW;
+	config.Pin = Pins;
+	HAL_GPIO_Init(keypad->IntputPort, &config);
+
+//turn off all the outputs
+	Keypad_SetOutputs(keypad, 0);
+}
---
+++ Drivers/Device_Drivers/Matrix_Keypad/Matrix_Keypad.h
@@ -0,0 +1,52 @@
+/*
+ * Matrix_Keypad.h
+ *
+ *  Created on: Jan 31, 2024
+ *      Author: ramys
+ */
+
+#ifndef DEVICE_DRIVERS_MATRIX_KEYPAD_H_
+#define DEVICE_DRIVERS_MATRIX_KEYPAD_H_
+
+#include <stdint.h>
+
+//this is where the MCAL drivers are called
+#include "stm32f1xx_hal.h"
+
+typedef struct {
+	/// @brief defines the number of rows
+	uint8_t Rows;
+
+	/// @brief defines the number of columns
+	uint8_t Columns;
+
+	/// @brief Holds the status of all keys
+	/// 32-bits means the max number of keys is 32
+	uint32_t KeysStatus;
+
+	/// @brief the GPIO handle where the input pins are attached
+	GPIO_TypeDef *IntputPort;
+
+	/// @brief the starting index of the first input pin
+	uint32_t InputStartingPin;
+
+	/// @brief The GPIO Handle where the output pins are attached
+	GPIO_TypeDef *OutputPort;
+
+	/// @brief the starting index of the first Output Pin
+	uint32_t OutputStartingPin;
+
+	/// @brief internally used to hold the 1s mask used for IO operations
+	uint16_t IP_Mask;
+	uint16_t OP_Mask;
+} Matrix_Keypad_t;
+
+/// @brief Configres the hardware to use keypad
+/// @param keypad
+void Keypad_Init(Matrix_Keypad_t *keypad);
+
+/// @brief Function periodically called by the user to refresh the status of the keys
+/// @param keypad
+void Keypad_Refresh(Matrix_Keypad_t *keypad);
+
+#endif /* DEVICE_DRIVERS_MATRIX_KEYPAD_H_ */
